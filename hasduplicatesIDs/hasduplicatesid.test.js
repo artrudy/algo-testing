1d589b1bf469678ff71e1c42c754fcc07058613c
artёm rudoy <art.rudy@gmail.com>
2023-10-22 22:35:36 +0300

    code optimization (beforeEach and afterEach)

--- hasduplicatesIDs/hasduplicatesid.test.js
+++ hasduplicatesIDs/hasduplicatesid.test.js
@@ -1,6 +1,20 @@
 const hasDuplicatesIds = require("./hasduplicatesid");
 
 describe("DOM Three has dublicates IDs", () => {
+  let root;
+  beforeEach(() => {
+    root = document.createElement("div");
+    const child1 = document.createElement("div");
+    const child2 = document.createElement("div");
+
+    root.appendChild(child1);
+    root.appendChild(child2);
+  });
+
+  afterEach(() => {
+    root = null;
+  });
+
   it("should be a function", () => {
     expect(typeof hasDuplicatesIds).toEqual("function");
   });
@@ -12,16 +26,9 @@ describe("DOM Three has dublicates IDs", () => {
   });
 
   it("should return true if there are duplicate IDs", () => {
-    const root = document.createElement("div");
-    const child1 = document.createElement("div");
-    const child2 = document.createElement("div");
-
-    root.appendChild(child1);
-    root.appendChild(child2);
-
     root.id = "root";
-    child1.id = "child";
-    child2.id = "child";
+    root.children[0].id = "child";
+    root.children[1].id = "child";
 
     const result = hasDuplicatesIds(root);
 
@@ -29,16 +36,9 @@ describe("DOM Three has dublicates IDs", () => {
   });
 
   it("should return false if there are no duplicate IDs", () => {
-    const root = document.createElement("div");
-    const child1 = document.createElement("div");
-    const child2 = document.createElement("div");
-
-    root.appendChild(child1);
-    root.appendChild(child2);
-
     root.id = "root";
-    child1.id = "child1";
-    child2.id = "child2";
+    root.children[0].id = "child1";
+    root.children[1].id = "child2";
 
     const result = hasDuplicatesIds(root);
 
